86608c05ae54cf155c834427c3812a54adeb2a59
hernansaavedrag <hernan.saavedra@gmail.com>
2025-08-13 11:31:23 -0400

    practica 1 parte 1

---
+++ Ejemplo2_301_2025/Ejemplo2_301_2025/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo2_301_2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            
+
+            int num1 = 4, num2 = 5, num6;
+            int suma;
+            string nombre = "Profe Hernán";
+            bool esCSharp = true;
+            float num3 = 3.5f;
+            decimal num4 = 2.5m;
+            string num5 = "14";
+
+            suma = num1 + num2 + (int)num3 + (int)num4 + int.Parse(num5);
+
+            //cw + tab + tab 
+            Console.WriteLine("la suma es: " +suma);
+            Console.WriteLine("Hola "+ nombre);
+            Console.WriteLine("Lenguaje C#: "+esCSharp);
+
+            Console.Write("Ingrese un valor para num6: ");
+            //num6 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out num6);
+
+            suma += num6; //acumulador --> suma = suma + num6
+            Console.WriteLine("la suma es: " + suma);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practica1_301_2025/Practica1_301_2025/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1_301_2025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear(); // aplica toda la consola
+
+            Console.Write("Ingrese un numero: ");
+            texto = Console.ReadLine();
+
+            if (texto == null){
+                texto = string.Empty;
+            }
+            int largo = texto.Length;
+
+            if (largo % 2 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            for (int i = 0; i < largo; i++)
+            {
+                Console.WriteLine(texto);
+            }
+
+            Console.ResetColor();
+           
+            Console.WriteLine();
+            Console.WriteLine("El largo del texto es: "+largo);
+
+            Console.ReadLine();
+        }
+    }
+}
