c9d2e940bf2439f15b2001b5927115bac19501c2
hernansaavedrag <hernan.saavedra@gmail.com>
2025-08-13 13:39:37 -0400

    guia practica 1 hasta ejercico 6 - clase 13-08-2025

--- Practica1_301_2025/Practica1_301_2025/Program.cs
+++ Practica1_301_2025/Practica1_301_2025/Program.cs
@@ -42,6 +42,70 @@ namespace Practica1_301_2025
             Console.WriteLine();
             Console.WriteLine("El largo del texto es: "+largo);
 
+            //ejercicio 5.
+            Console.WriteLine("Ingrese un numero entero: ");
+            string numeros = Console.ReadLine();
+
+            int suma = 0;
+
+            foreach (char num in numeros)
+            {
+                if (char.IsDigit(num)) //solo sumando si es un número
+                {
+                    /*string palabra = "" + 2; --> convierte el numero a string
+                    string pala = 2 + "";
+                    Console.WriteLine(pala + 4);
+                    int dato = 3;
+                    Console.WriteLine(dato +4);
+                    */
+                    int digito = num - '0'; //convierte char a int
+                    suma = suma + digito; //suma += digito;
+                    //"45"
+                    //'4' - '0' --> 4
+                    //'5' - '0' --> 5
+                }
+            }
+
+            Console.WriteLine("La suma de los digitos es: "+suma);
+            // --- fin ejercicio 5 -----
+
+            // ---- ejercicio 6 ------
+
+            Console.WriteLine("Ingrese una cantidad numérica: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            List<int> pares = new List<int>();
+            List<int> impares = new List<int>();
+            List<int> primos = new List<int>();
+
+            int numero = 1;
+            
+            while (numero <= cantidad) //|| impares.Count < cantidad )//|| primos.Count < cantidad)
+            {
+                //pares
+                if (numero%2 == 0 && pares.Count < cantidad)
+                {
+                    pares.Add(numero);
+                }
+               
+                //impares
+                else
+                    Console.WriteLine("hola par");
+                {
+                   if (numero % 2 != 0 && impares.Count < cantidad)
+                    {
+                        impares.Add(numero);
+                    }
+                }
+                numero++;
+            }
+
+            Console.WriteLine("Numeros Pares: " + pares.Count);
+            Console.WriteLine("Numeros imPares: " + impares.Count);
+
+            // ---- fin jercicio 6 ----
+
+
             Console.ReadLine();
         }
     }
